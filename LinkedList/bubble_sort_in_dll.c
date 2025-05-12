749e5f87580e51e954f77cf8a6749d4b27f78e88
nirwaniadhau <adhaunirwani65@gmail.com>
2025-05-12 21:20:35 +0530

    Create bubble_sort_in_dll.c

---
+++ LinkedList/bubble_sort_in_dll.c
@@ -0,0 +1,87 @@
+        #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+// Function to insert at the end of the list
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+// Function to print the list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Bubble Sort for Doubly Linked List
+void bubbleSort(struct Node* head) {
+    int swapped = 1;
+    while (swapped) {
+        swapped = 0;
+        struct Node* current = head;
+
+        while (current != NULL && current->next != NULL) {
+            if (current->data > current->next->data) {
+                // Swap data
+                int temp = current->data;
+                current->data = current->next->data;
+                current->next->data = temp;
+                swapped = 1;
+            }
+            current = current->next;
+        }
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, value;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &value);
+        insertEnd(&head, value);
+    }
+
+    printf("Original List:\n");
+    printList(head);
+
+    bubbleSort(head);
+
+    printf("Sorted List:\n");
+    printList(head);
+
+    return 0;
+}
